b6827e60fcb781b9447aab85b153d9410c884d92
Lane Aasen <laneaasen@gmail.com>
2012-11-07 23:11:50 -0800

    validates ideas

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -4,4 +4,9 @@ class Idea < ActiveRecord::Base
   belongs_to :user
 
   attr_accessible :name, :content, :user, :tag_list
+
+  validates :name, :presence => true,
+    :length => { :maximum => 64 }
+  validates :content, :allow_blank => true,
+    :length => { :maximum => 512 }
 end
