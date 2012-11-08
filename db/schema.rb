f8cc2e19f8ac36aa24f7c3ebfec873a9c57187e3
Lane Aasen <laneaasen@gmail.com>
2012-11-08 09:33:29 -0800

    adds barebones user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121108172139) do
+ActiveRecord::Schema.define(:version => 20121108173121) do
 
   create_table "ideas", :force => true do |t|
     t.string   "name"
@@ -39,10 +39,9 @@ ActiveRecord::Schema.define(:version => 20121108172139) do
   end
 
   create_table "users", :force => true do |t|
-    t.integer  "created",    :limit => 8
-    t.string   "username"
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
